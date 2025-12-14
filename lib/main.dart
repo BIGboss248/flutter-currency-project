85b7188bf35c64ca0bc2b0d69925110197b15b2d
bigboss248 <aminjamali79@gmail.com>
2025-12-14 15:20:23 +0330

    added todo

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'dart:developer' as developer;
 
 // GoRouter configuration
 final _router = GoRouter(
+  /* TODO Implement route protection */
   routes: [
     GoRoute(
       path: mainPageRoute,

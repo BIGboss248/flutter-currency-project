12423e16873bcc12fe90762a2f35cc8e166fdb07
bigboss248 <aminjamali79@gmail.com>
2025-12-14 11:51:59 +0330

    Fixed UI update

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -19,10 +19,12 @@ class _NotesState extends State<Notes> {
   late final NotesService _noteService;
   final List<Widget> items = []; // MUST NOT be const
   String get userEmail => AuthService.firebase().currentUser!.email!;
+  Future<DatabaseUser>? dbUser;
 
   @override
   void initState() {
     _noteService = NotesService();
+    dbUser = _noteService.getOrCreateUser(email: userEmail);
     super.initState();
   }
 
@@ -51,7 +53,7 @@ class _NotesState extends State<Notes> {
         child: Column(
           children: [
             FutureBuilder(
-              future: _noteService.getOrCreateUser(email: userEmail),
+              future: dbUser,
               builder: (context, snapshot) {
                 switch (snapshot.connectionState) {
                   case ConnectionState.done:
@@ -61,13 +63,21 @@ class _NotesState extends State<Notes> {
                         switch (snapshot.connectionState) {
                           case ConnectionState.waiting:
                             return const Text("Waiting for all notes");
-                          default:
-                            return CircularProgressIndicator();
+                          case ConnectionState.done:
+                            return const Text("Stream is done");
+                          case ConnectionState.active:
+                            return const Text("Stream is active");
+                          case ConnectionState.none:
+                            return const Text("Stream is none");
                         }
                       },
                     );
-                  default:
+                  case ConnectionState.waiting:
                     return CircularProgressIndicator();
+                  case ConnectionState.none:
+                    return Text("Future builder is none");
+                  case ConnectionState.active:
+                    return Text("Future builder is active");
                 }
               },
             ),

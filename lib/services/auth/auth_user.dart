bf64e8004dd698cc40bc487821453aa30b44e59a
bigboss248 <aminjamali79@gmail.com>
2025-12-17 11:16:26 +0330

    Added UId to user

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -9,10 +9,11 @@ import 'package:flutter/foundation.dart'; // For class tags
 /* TODO Check class flags */
 @immutable
 class AuthUser {
+  final String id;
   final bool isEmailVerified;
   final String? email;
   get getEmail => email;
-  const AuthUser({required this.isEmailVerified, required this.email});
+  const AuthUser({required this.isEmailVerified, required this.email, required this.id});
   factory AuthUser.fromFireBase(User user) =>
-      AuthUser(isEmailVerified: user.emailVerified, email: user.email);
+      AuthUser(isEmailVerified: user.emailVerified, email: user.email, id: user.uid);
 }

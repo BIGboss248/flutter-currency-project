be861ffb41ddf61d61a0f918eea173d419c794cc
BIGboss248 <aminjamali79@gmail.com>
2025-12-24 18:47:32 +0330

    Added logging messages to Firebase auth provider

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -1,6 +1,7 @@
 /*
   In this file we create firebase authProvide for our authService to use
 */
+import 'package:budgee/utils/app_logger.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:budgee/firebase_options.dart';
 import 'package:budgee/services/auth/auth_provider.dart';
@@ -16,6 +17,7 @@ class FireBaseAuthProvider implements AuthProvider {
     required String password,
   }) async {
     try {
+      logger.i("Creating user with email: $email");
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
@@ -24,17 +26,22 @@ class FireBaseAuthProvider implements AuthProvider {
       if (user != null) {
         return user;
       } else {
+        logger.e("Failed to create a user $email");
         throw UserNotLoggedInAuthException();
       }
     } on FirebaseAuthException catch (e) {
       switch (e.code) {
         case "email-already-in-use":
+          logger.e("Email $email already in use");
           throw EmailAlreadyInUseAuthExecption();
         case "weak-password":
+          logger.e("User $email failed to subscription with weak password");
           throw WeakPasswordAuthExecption();
         case "invalid-email":
+          logger.e("User failed to sign up with invalid email$email");
           throw InvalidEmailAuthExecption();
         default:
+          logger.e("Generic error occurred while creating user $email");
           throw GenericAuthExecption();
       }
     } catch (e) {
@@ -48,6 +55,7 @@ class FireBaseAuthProvider implements AuthProvider {
     required String password,
   }) async {
     try {
+      logger.i("Logging in user with email: $email");
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: email,
         password: password,
@@ -56,15 +64,19 @@ class FireBaseAuthProvider implements AuthProvider {
       if (user != null) {
         return user;
       } else {
+        logger.e("Failed to log in user $email");
         throw UserNotLoggedInAuthException();
       }
     } on FirebaseAuthException catch (e) {
       switch (e.code) {
         case "wrong-password":
+          logger.e("User $email failed to login with wrong password");
           throw WrongPasswordAuthExecption();
         case "user-not-found":
+          logger.e("User with email $email not found");
           throw UserNotFoundAuthExecption();
         case "invalid-email":
+          logger.e("User failed to login with invalid email $email");
           throw InvalidEmailAuthExecption();
         default:
           throw GenericAuthExecption();
@@ -76,10 +88,12 @@ class FireBaseAuthProvider implements AuthProvider {
 
   @override
   Future<void> logOut() async {
+    logger.i("Logging out user");
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
       await FirebaseAuth.instance.signOut();
     } else {
+      logger.e("Failed to log out user because no user is logged in");
       throw UserNotLoggedInAuthException();
     }
   }
@@ -88,8 +102,12 @@ class FireBaseAuthProvider implements AuthProvider {
   Future<void> sendEmailVerification() async {
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
+      logger.i("Sending email verification to user ${user.email}");
       user.sendEmailVerification();
     } else {
+      logger.e(
+        "Failed to send email verification because no user is logged in",
+      );
       throw UserNotLoggedInAuthException();
     }
   }
@@ -105,6 +123,7 @@ class FireBaseAuthProvider implements AuthProvider {
 
   @override
   Future<void> initialize() async {
+    logger.i("Initializing firebase auth provider...");
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );

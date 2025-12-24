b34190a4573fbaad7c05f08d4e566f5a0f35b536
BIGboss248 <aminjamali79@gmail.com>
2025-12-24 18:56:40 +0330

    Added logging

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -5,6 +5,7 @@ import 'package:budgee/pages/notes_page.dart';
 import 'package:budgee/pages/regiteration_page.dart';
 import 'package:budgee/pages/verify_email_page.dart';
 import 'package:budgee/services/auth/auth_service.dart';
+import 'package:budgee/utils/app_logger.dart';
 import 'package:go_router/go_router.dart';
 
 const String mainPageRoute = '/';
@@ -20,17 +21,24 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: mainPageRoute,
-      builder: (context, state) => HomePage(pageIndex: 0),
+      builder: (context, state) {
+        logger.i("Navigating to Home page");
+        return HomePage(pageIndex: 0);
+      },
     ),
     GoRoute(
       path: notesPageRoute,
-      builder: (context, state) => Notes(pageIndex: 1),
+      builder: (context, state) {
+        logger.i("Navigating to Notes page");
+        return Notes(pageIndex: 1);
+      },
       redirect: (context, state) {
         final user = AuthService.firebase().currentUser;
         final isLoggedIn = user != null;
 
         // Redirect to login if not authenticated
         if (!isLoggedIn) {
+          logger.i("Redirecting from notes page to login page");
           return '$loginPageRoute?fromSourcePage=${Uri.encodeComponent(state.uri.toString())}';
         }
 
@@ -42,26 +50,32 @@ final router = GoRouter(
       builder: (context, state) {
         final fromSourcePage = state.uri.queryParameters['fromSourcePage'];
         if (fromSourcePage != null) {
+          logger.i("Redirecting from login page to notes page");
           return LoginPage(pageIndex: 2, fromSourcePage: fromSourcePage);
         }
-        
+
         return LoginPage(pageIndex: 2);
       },
     ),
     GoRoute(
       path: registerPageRoute,
-      builder: (context, state) => RegisterationPage(pageIndex: 3),
+      builder: (context, state) {
+        logger.i("Navigating to Registeration page");
+        return RegisterationPage(pageIndex: 3);
+      },
     ),
     GoRoute(
       path: verifyEmailPageRoute,
       builder: (context, state) {
         final email = state.uri.queryParameters['email'];
+        logger.i("Navigating to Verify Email page for email: $email");
         return VerifyEmailPage(pageIndex: 4, email: email);
       },
     ),
     GoRoute(
       path: newNoteRoute,
       builder: (context, state) {
+        logger.i("Navigating to New Note page");
         final noteId = state.uri.queryParameters['noteId'];
         if (noteId == null) {
           return NewNote(pageIndex: 5);
@@ -73,6 +87,9 @@ final router = GoRouter(
 
         // Redirect to login if not authenticated
         if (!isLoggedIn) {
+          logger.i(
+            "Redirecting from new note page to login page user is note logged in",
+          );
           return '$loginPageRoute?fromSourcePage=${Uri.encodeComponent(state.uri.toString())}';
         }
 
